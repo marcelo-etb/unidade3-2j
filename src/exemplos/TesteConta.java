c004d7beb91891fa71f59b07d8851fb2c89005cc
marcelo-etb <mns.etb@gmail.com>
2024-05-03 22:27:30 -0300

    Utilizando metodos set nas linhas 8 e 9 e toString na linha 17

--- src/exemplos/TesteConta.java
+++ src/exemplos/TesteConta.java
@@ -5,12 +5,15 @@ public class TesteConta {
         Conta c = new Conta();
         c.depositar(50);
         c.depositar(100);
-        boolean saqueEfetuado = c.sacar(80);
+        c.setNomeTitular("Marcelo");
+        c.setNumero(123456);
+        boolean saqueEfetuado = c.sacar(800);
         if (saqueEfetuado) {
             JOptionPane.showMessageDialog(null, "Saque efetuado com sucesso!");
         } else {
             JOptionPane.showMessageDialog(null, "Erro ao efetuar saque");
         }
-        JOptionPane.showMessageDialog(null, "Saldo atual: " + c.saldo);
+        // JOptionPane.showMessageDialog(null, "Saldo atual: " + c.getSaldo());
+        JOptionPane.showMessageDialog(null, c);
     }
 }
